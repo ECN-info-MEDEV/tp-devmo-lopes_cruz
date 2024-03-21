c12ea6250409db502a0e01c80684769dd57746c0
Luan Lopes Fontes <l239682@dac.unicamp.br>
2024-03-21 09:26:36 +0100

    update form_profil class

---
+++ app/src/main/java/com/example/brainmatch/ui/profileParams/FormProfil.kt
@@ -0,0 +1,9 @@
+package com.example.brainmatch.ui.profileParams
+
+data class Form(
+    val university: String,
+    val degree: String,
+    val academicYear: String,
+    val specialization: String,
+    val city: String
+)
