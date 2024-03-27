d286d33cc0e7f67b7a111faf2c523c3437c66bd3
Sacha2023 <sachacruzabsi@gmail.com>
2024-03-27 23:43:33 +0100

    Fixed almost every issues.
    Missing the cursor being stuck to the left

--- app/src/main/java/com/example/brainmatch/ui/profileParams/ProfileParamsFragment.kt
+++ app/src/main/java/com/example/brainmatch/ui/profileParams/ProfileParamsFragment.kt
@@ -11,7 +11,6 @@ import android.text.Editable
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import android.widget.EditText
-import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.brainmatch.R
@@ -33,8 +32,7 @@ class ProfileParamsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val profileParamsViewModel =
-            ViewModelProvider(this).get(ProfileParamsViewModel::class.java)
+        val profileParamsViewModel = ViewModelProvider(requireActivity()).get(ProfileParamsViewModel::class.java)
 
         _binding = FragmentProfileParamsBinding.inflate(inflater, container, false)
         val root: View = binding.root
@@ -45,15 +43,18 @@ class ProfileParamsFragment : Fragment() {
         academicYearEditText = binding.academicYearEditText
         specializationEditText = binding.specializationEditText
         cityEditText = binding.cityEditText
-        // Watches TODO TODO TODO
+
         val formTextWatcher = object : TextWatcher {
             var oldText: String = ""
+
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
                 oldText = s.toString()
             }
+
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
                 // This space intentionally left blank
             }
+
             override fun afterTextChanged(s: Editable) {
                 if (oldText != s.toString()) {
                     profileParamsViewModel.updateForm(
@@ -66,12 +67,14 @@ class ProfileParamsFragment : Fragment() {
                 }
             }
         }
+
         // Add the FormTextWatcher to each EditText
         universityEditText.addTextChangedListener(formTextWatcher)
         degreeEditText.addTextChangedListener(formTextWatcher)
         academicYearEditText.addTextChangedListener(formTextWatcher)
         specializationEditText.addTextChangedListener(formTextWatcher)
         cityEditText.addTextChangedListener(formTextWatcher)
+
         // Observe changes in the form data
         profileParamsViewModel.form.observe(viewLifecycleOwner) { form ->
             universityEditText.setText(form.university)
@@ -81,33 +84,28 @@ class ProfileParamsFragment : Fragment() {
             cityEditText.setText(form.city)
         }
         // Add click listener to navigate to profile params
-        setupButton(R.id.button_profile)
+        binding.buttonProfile.setOnClickListener { view ->
+            setupButton()
+            onReturnClick(view)
+        }
         return root
     }
 
-    private fun setupButton(buttonId: Int) {
-        // Get reference to the button
+    private fun setupButton() {
+        val buttonId = R.id.button_profile
         val button = binding.root.findViewById<Button>(buttonId)
-        button.setOnClickListener {
-            // Change the button background to green when clicked
-            button.setBackgroundResource(R.drawable.rounded_button_highlight)
-            // After a delay, change the button background back to black
-            Handler(Looper.getMainLooper()).postDelayed({
-                button.setBackgroundResource(R.drawable.rounded_button)
-                if (buttonId == R.id.button_profile) {
-                    // If the profile button was clicked, navigate to the profile page
-                    findNavController().navigate(R.id.navigation_profile)
-                }
-            }, 150)
-        }
+        button.setBackgroundResource(R.drawable.rounded_button_highlight)
+        Handler(Looper.getMainLooper()).postDelayed({
+            button.setBackgroundResource(R.drawable.rounded_button)
+        }, 150)
+    }
+
+    private fun onReturnClick(view: View) {
+        findNavController().navigate(R.id.navigation_profile)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    private fun onReturnClick() {
-        findNavController().navigate(R.id.navigation_profile)
     }
 }
